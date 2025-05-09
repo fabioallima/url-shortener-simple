59e2ee734ec09e3d4c2d968e0ac4b0eda447dd88
Fabio Almeida Lima <fabio.allima@hotmail.com>
2025-05-09 12:10:34 -0300

    create terraform

---
+++ infra/main.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_dynamodb_table" "url_shortener" {
+  name         = "url-shortener-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda-url-shortener-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attach" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_exec" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "create_url_shortener" {
+  function_name = "create-url-shortener"
+  runtime       = "python3.11"
+  handler       = "lambda_function.lambda_handler"
+  role          = aws_iam_role.lambda_exec_role.arn
+  timeout       = 10
+
+  filename         = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.url_shortener.name
+    }
+  }
+}
---
+++ infra/outputs.tf
@@ -0,0 +1,3 @@
+output "lambda_function_name" {
+  value = aws_lambda_function.create_url_shortener.function_name
+}
