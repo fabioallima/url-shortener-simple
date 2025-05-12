85a35a11a0b3d3547a9b24f926ee7fd2966c89db
Fabio Almeida Lima <fabio.allima@hotmail.com>
2025-05-12 12:27:49 -0300

    terraform force-unlock test

--- infra/s3.tf
+++ infra/s3.tf
@@ -40,20 +40,3 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-# Tabela DynamoDB para bloqueio de estado
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform-state-lock-${var.environment}"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "Terraform State Lock"
-    Environment = var.environment
-  }
-}
